0b1a1fcc20e565a03ba00f552a58e51385c71a30
Palak Agarwal <115281965+palakagarwal081@users.noreply.github.com>
2025-04-12 13:35:30 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,24 @@
 # variable "location" {
 #   default = "East US"
 # }
+variable "client_id" {
+  description = "Azure Active Directory Application Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "client_secret" {
+  description = "Azure Active Directory Application Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Active Directory Tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
